1f5d0978471907493041eb19aa0900db16f1c124
Vicki <vicki@datamonk.net>
2021-09-20 21:51:17 +1000

    fix repo

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 engine = create_engine("postgresql://qttgloeftjasgq:6ee694584f865888de3969ea9c6196a74435377c44b382d0ab4a8a3f529fe0c4@ec2-34-234-12-149.compute-1.amazonaws.com:5432/dfofff6l7k23vb")
 
